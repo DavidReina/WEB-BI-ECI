22e0f8dd9ac03659ebf61ee889dca48bc5682716
Ludwing Acevedo <ludwing.acevedo@mail.escuelaing.edu.co>
2017-04-01 13:01:49 -0500

    Avance profesor2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,10 +5,10 @@ function connect() {
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        
+
         stompClient.subscribe('/topic/TOPICOXX', function (data) {
-           
-            
+
+
         });
     });
 }
@@ -21,14 +21,25 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function direccionamiento(){
+direccionamiento = function () {
     //var tipo = document.getElementById("#profesor").value;
     //console.log("hola"+tipo);
-    console.log("no");
-    location.href = "profesor.html";
-    alert("pas");
-    location.href = "profesor.html";
-}
+    if (document.getElementById("tipop").value == "1") {
+
+    } else
+        location.href = "profesor.html";
+};
+
+datosProfesor = function () {
+
+    Nombre = document.getElementById("nombre").value;
+    TI = document.getElementById("nti").value;
+    Carnet = document.getElementById("carnet").value;
+    Correo = document.getElementById("correo").value;
+    console.log("Holaaa");
+
+};
+
 
 
 $(document).ready(
