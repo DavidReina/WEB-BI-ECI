198903bcd6c623ef039d1dced241d5a334ce16d8
David Reina <david.reina@mail.escuelaing.edu.co>
2017-04-01 12:35:46 -0500

    integraciones y test

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,32 @@
+
+import org.junit.Test;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2103216
+ */
+
+
+public class AppTest {
+    /*
+    Clase de Equivalencia: Se reciben los datos de un profesor para generar un registro adecuado con su nombre y usuario,
+    es decir aquellos usuarios que sean profesores
+    Condiciones de Frontera: al ingresar a la plataforma se genera un usuario de tipo profesor con valores nullos , es decir ha yque grantizar
+    que todos los valores sean insertados correctamente y se puedan validar
+    SE ESPERA UN ERROR: SI()  NO(X)
+    TIPO: DE FRONTERA
+    
+    */
+    
+    @Test
+     public void RegistrarProfesor(){
+         
+     }
+}
